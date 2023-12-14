4fa33f0a8cf750734bf7f1bb5f52b0630430c8e8
凌览 <lihai_js@163.com>
2023-12-14 15:06:28 +0800

    feat: 添加一个停机维护的页面

---
+++ src/pages/downtime/downtime.vue
@@ -0,0 +1,14 @@
+<template>
+    <view class="downtime">
+        <image src="https://ywja-public-bucket.oss-cn-hangzhou.aliyuncs.com/jingyu/weihu.webp" style="width:600rpx;"></image>
+    </view>
+</template>
+<style lang="scss" scoped>
+.downtime {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    text-align: center;
+    transform: translate(-50%, -50%);
+}
+</style>
