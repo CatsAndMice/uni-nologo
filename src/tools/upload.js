de4518243457aac56cc9ad5e88fd59fb0402a7fb
凌览 <lihai_js@163.com>
2025-09-19 14:33:53 +0800

    refactor(upload): 重构上传功能
    - 移除 OSS 配置获取，改为直接使用后端接口上传
    - 更新上传 URL 和请求头
    - 添加用户认证 token
    - 修改文件上传成功后的处理逻辑
    - 优化代码格式和变量命名

--- src/tools/upload.js
+++ src/tools/upload.js
@@ -5,52 +5,78 @@ import {
 	isUndefined,
 	isFunction
 } from '@/tools/tool.js'
-export const uploadImages = async (images,callback) => {
-	const {
-		code,
-		data
-	} = await getOssConfig()
-	if(!(code==200)){
-		callback({status:false, index:-1})
-		return
-	} 
-	const config = data
+import { baseURL } from '@/config/app'
+import {
+	userData
+} from '@/stores/userData.js'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const uploadImages = async (images, callback) => {
+	// const {
+	// 	code,
+	// 	data
+	// } = await getOssConfig()
+	// if (!(code == 200)) {
+	// 	callback({ status: false, index: -1 })
+	// 	return
+	// }
+	// const config = data
 	for (let i = 0; i < images.tempFilePaths.length; i++) {
-	
+
 		const path = images.tempFilePaths[i]
 		const splitValues = path.split('/')
-		const nameValues =splitValues[splitValues.length - 1]
+		const nameValues = splitValues[splitValues.length - 1]
 		const nameValue = nameValues.split('.')
 		const name = `${nameValue[0]}.png`
-		const key = `${config.prefixFormat}${name}`
+		// const key = `${config.prefixFormat}${name}`
+
 		uni.uploadFile({
-			url: config.host,
+			url: baseURL + '/dian/apis/oss/upload/direct',
 			filePath: path,
 			fileType: 'image',
 			name: 'file',
+			header: {
+				'Content-Type': 'multipart/form-data',
+				authorization: userData().token
+			},
 			formData: {
-				'policy': config.policy,
-				'OSSAccessKeyId': config.accessId,
-				'signature': config.signature,
-				'success_action_status': 200,
-				key,
-				name
+				isPrivate: false
 			},
 			success: (uploadFileRes) => {
 				const code = uploadFileRes.statusCode
 				if (code == 200) {
-					const url = config.host + key;
-					isFunction(callback) && callback({
-						status:true,
-						index:i,
-						url:url,
-						name:name
-					})
+					const data = JSON.parse(uploadFileRes.data);
+					if (data.code = 200) {
+						console.log(data.data.fileKey);
+						
+						isFunction(callback) && callback({
+							status: true,
+							index: i,
+							url: `https://jingobj.jasolar.com:26000/${process.env.UNI_NODE_ENV == 'prod?' ? 'ywja-public-bucket' : 'ja-tdd'}/${data.data.fileKey}`,
+							name: name
+						})
+					}
+
 				}
 			},
 			fail(err) {
-				console.log(err);
-				isFunction(callback) && callback({staus:false,index:i})
+
+				isFunction(callback) && callback({ staus: false, index: i })
 			}
 		});
 	}
